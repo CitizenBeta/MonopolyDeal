b42ddc518506ee57f62e277ea7b70a46596336c6
REPENTANCE-BAEee <3544960181@qq.com>
2026-05-12 10:27:40 +0800

    v0.13.1

--- src/main/java/ie/ucd/monopolydeal/model/Player.java
+++ src/main/java/ie/ucd/monopolydeal/model/Player.java
@@ -2,6 +2,7 @@ package ie.ucd.monopolydeal.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Player {
     public static final int MAX_CARDS_AT_HAND = 7;
@@ -11,6 +12,7 @@ public class Player {
     private final int number;
     private final List<Card> cardsAtHand = new ArrayList<>();
     private final List<Card> cardsAtBank = new ArrayList<>();
+    private final List<PropertySet> propertySets = new ArrayList<>();
 
     public Player(String name, int number) {
         this.name = name;
@@ -34,32 +36,51 @@ public class Player {
     }
 
     public void addCardToHand(Card card) {
-        if (card == null) {
-            return;
-        }
+        if (card == null) return;
         cardsAtHand.add(card);
     }
 
     public void addCardToBank(Card card) {
-        if (card == null) {
-            return;
-        }
+        if (card == null) return;
         cardsAtHand.remove(card);
-        cardsAtBank.add(card);
+        if (card instanceof PropertyCard || card instanceof WildPropertyCard) {
+            PropertyColor targetColor = getTargetColorForCard(card);
+            PropertySet set = findCompatibleSet(targetColor);
+            if (set != null && set.addCard(card)) {
+                cardsAtBank.add(card);
+                return;
+            }
+            int required = getRequiredCountForColor(targetColor);
+            PropertySet newSet = new PropertySet(targetColor, required);
+            if (newSet.addCard(card)) {
+                propertySets.add(newSet);
+                cardsAtBank.add(card);
+            }
+        } else {
+            cardsAtBank.add(card);
+        }
     }
 
     public boolean removeCardFromHand(Card card) {
-        if(card == null){
-            return false;
-        }
+        if (card == null) return false;
         return cardsAtHand.remove(card);
     }
 
     public boolean removeCardFromBank(Card card) {
-        if(card == null){
-            return false;
+        if (card == null) return false;
+        boolean removed = cardsAtBank.remove(card);
+        if (removed) {
+            for (PropertySet ps : propertySets) {
+                if (ps.getCards().contains(card)) {
+                    ps.removeCard(card);
+                    if (ps.getCurrentCount() == 0) {
+                        propertySets.remove(ps);
+                    }
+                    break;
+                }
+            }
         }
-        return cardsAtBank.remove(card);
+        return removed;
     }
 
     public int getBankTotalValue() {
@@ -72,5 +93,51 @@ public class Player {
 
     public boolean isHandFull() {
         return cardsAtHand.size() > MAX_CARDS_AT_HAND;
+    }
+
+    public List<PropertySet> getCompleteSets() {
+        return propertySets.stream().filter(PropertySet::isComplete).collect(Collectors.toList());
+    }
+
+    public List<PropertySet> getAllPropertySets() {
+        return new ArrayList<>(propertySets);
+    }
+
+    public int getTotalRent() {
+        return propertySets.stream().mapToInt(PropertySet::calculateRent).sum();
+    }
+
+    private PropertyColor getTargetColorForCard(Card card) {
+        if (card instanceof PropertyCard) {
+            return ((PropertyCard) card).getColor();
+        } else if (card instanceof WildPropertyCard) {
+            return ((WildPropertyCard) card).getPossibleColors().get(0);
+        }
+        return null;
+    }
+
+    private PropertySet findCompatibleSet(PropertyColor color) {
+        for (PropertySet ps : propertySets) {
+            if (ps.getColor() == color && !ps.isComplete()) {
+                return ps;
+            }
+        }
+        return null;
+    }
+
+    private int getRequiredCountForColor(PropertyColor color) {
+        switch (color) {
+            case BROWN: return 2;
+            case LIGHT_BLUE: return 3;
+            case PINK: return 3;
+            case ORANGE: return 3;
+            case RED: return 3;
+            case YELLOW: return 3;
+            case GREEN: return 3;
+            case DARK_BLUE: return 2;
+            case RAILROAD: return 4;
+            case UTILITY: return 2;
+            default: return 0;
+        }
     }
 }
--- src/main/java/ie/ucd/monopolydeal/model/PropertySet.java
+++ src/main/java/ie/ucd/monopolydeal/model/PropertySet.java
@@ -4,19 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PropertySet {
-    private final String color;
+    private final PropertyColor color;
     private final int requiredCount;
     private final List<Card> cards;
 
-    public PropertySet(String color, int requiredCount) {
+    public PropertySet(PropertyColor color, int requiredCount) {
         this.color = color;
         this.requiredCount = requiredCount;
         this.cards = new ArrayList<>();
     }
 
     public boolean addCard(Card card) {
-        if (card == null) return false;
-        if (cards.size() >= requiredCount) return false;
+        if (card == null || cards.size() >= requiredCount) return false;
+        if (card instanceof PropertyCard) {
+            if (((PropertyCard) card).getColor() != color) return false;
+        } else if (card instanceof WildPropertyCard) {
+            WildPropertyCard wild = (WildPropertyCard) card;
+            if (!wild.getPossibleColors().contains(color)) return false;
+        } else {
+            return false;
+        }
         return cards.add(card);
     }
 
@@ -28,7 +35,7 @@ public class PropertySet {
         return cards.size() == requiredCount;
     }
 
-    public String getColor() {
+    public PropertyColor getColor() {
         return color;
     }
 
@@ -42,5 +49,22 @@ public class PropertySet {
 
     public List<Card> getCards() {
         return new ArrayList<>(cards);
+    }
+
+    public int calculateRent() {
+        if (!isComplete()) return 0;
+        switch (color) {
+            case BROWN: return 1;
+            case LIGHT_BLUE: return 2;
+            case PINK: return 4;
+            case ORANGE: return 6;
+            case RED: return 8;
+            case YELLOW: return 10;
+            case GREEN: return 12;
+            case DARK_BLUE: return 16;
+            case RAILROAD: return 4 * cards.size();
+            case UTILITY: return cards.size() == 2 ? 10 : 4;
+            default: return 0;
+        }
     }
 }
